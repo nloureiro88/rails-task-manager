731f7612a537c55373df9c8fbce885db1124882c
Nuno Loureiro <nunoloureiro88@gmail.com>
2019-02-19 14:13:11 +0000

    Exercise solved with all tasks, including CSS, except for JavaScript alert.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(user_params)
+
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task = @task.update(user_params)
+
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def user_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
